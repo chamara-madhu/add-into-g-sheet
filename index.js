82682b593c07d927df41104af6596a1ec42aabc4
Chamara <chamara.m@eyepax.com>
2023-08-24 19:46:11 +0530

    update api

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const spreadsheetId = "1A-YybFvVcEWuSVAn11ClvUGYR9u-r570POlJgK9fp_Q";
 
 // Define a route that responds with "Hello, World!" when accessed
 app.post("/add-to-g-sheet", async (req, res) => {
+  const { email, answer1, answer2, answer3, summary } = req.body;
+
   console.log({ req });
   const auth = new google.auth.GoogleAuth({
     keyFile: "t-bounty-396905-f4fc0bf1a55e.json",
@@ -22,10 +24,10 @@ app.post("/add-to-g-sheet", async (req, res) => {
   const client = await auth.getClient();
   const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
+  //   const metaData = await googleSheets.spreadsheets.get({
+  //     auth,
+  //     spreadsheetId,
+  //   });
 
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
@@ -39,7 +41,13 @@ app.post("/add-to-g-sheet", async (req, res) => {
     range: "Sheet1",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [["chamara", 50]],
+      values: [
+        ["email", email],
+        ["answer1", answer1],
+        ["answer2", answer2],
+        ["answer3", answer3],
+        ["summary", summary],
+      ],
     },
   });
 

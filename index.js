d231c6cdc9945be1deb77058fa35efba017b45ff
Chamara <chamara.m@eyepax.com>
2023-08-24 19:50:12 +0530

    adding new script update

--- index.js
+++ index.js
@@ -41,13 +41,7 @@ app.post("/add-to-g-sheet", async (req, res) => {
     range: "Sheet1",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [
-        ["email", email],
-        ["answer1", answer1],
-        ["answer2", answer2],
-        ["answer3", answer3],
-        ["summary", summary],
-      ],
+      values: [[email, answer1, answer2, answer3, summary]],
     },
   });
 
